6b6be6b7864295b3f085a8adaaa1b92032b886a0
henry <henry@blackstormcompany.com>
2024-01-15 15:37:03 +0900

    Camera 관련 껍데기들... 아마도 안쓸지도?

---
+++ Plugins/UfCameraSystem/Source/UfCameraSystem/Public/UfCameraBoomComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "UfCameraBoomComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UFCAMERASYSTEM_API UUfCameraBoomComponent : public USpringArmComponent
+{
+	GENERATED_BODY()
+	
+};
